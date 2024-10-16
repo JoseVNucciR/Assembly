595ce62731b8ab24a11accb75c18538289338070
José Vitor <jvnuccirodrigues@gmail.com>
2024-10-16 16:54:59 -0300

    atualização

--- ATIV8_XLAT/ATIV8_4XLAT.asm
+++ ATIV8_XLAT/ATIV8_4XLAT.asm
@@ -1,43 +1,31 @@
-TITLE Saída de números hexadecimais
+TITLE Saída de números hexadecimais de XISLAT
 .MODEL SMALL
 .STACK 100h
 .DATA
     ARRAY db 30h,31h,32h,33h,34h,35h,36h,37h,38h,39h,41h,42h,43h,44h,45h,46h  
 .CODE
 MAIN PROC
+    ;permite acesso as variáveis de .DATA
     MOV AX,@DATA
     MOV DS,AX
-
-    MOV BX,067BEh
-    MOV CH,4
-    MOV AH,2
-    
+    MOV BX,067BEh ;Define um valor em BX
+    MOV CH,4 ;Adiciona o valor 4 ao contador CX
+    MOV AH,2 ;Função de impressão do INT 21h
 POGGERS: 
-    MOV DL,BH
-    PUSH BX
-    SHR DL,4
-    LEA BX,ARRAY
-    MOV AL,DL
+    MOV DL,BH ;salva os 8 bits de BH (67h) em DL
+    PUSH BX ;empurra BX para o topo da pilha
+    SHR DL,4 ;desloca o 7h para a direita para imprimir somente o 6h
+    LEA BX,ARRAY ;entra no array e troca seu valor pelo seu respectivo caracter com o XLAT
+    MOV AL,DL ;move DL para AL
     XLAT
-    POP BX
-    MOV DL,AL
-    INT 21h
-    ROL BX,4
-    DEC CH
+    POP BX ;pega de volta o antigo valor de BX
+    MOV DL,AL ;volta o valor de AL para DL para poder ser impresso
+    INT 21h ;executa a função AX,2 de impressão
+    ROL BX,4 ; rola os quatro primeiros bits (6h) para poder imprimir os próximos números hexa
+    DEC CH  ;decrementa 1 de CH até todos os números serem impressos
     JNZ POGGERS
-    
-    MOV AH,4CH
+    MOV AH,4Ch ;finaliza o código
     INT 21h
-
 MAIN ENDP 
 END MAIN
 
-
-
-
-
-
-
-
-MAIN ENDP
-END MAIN
