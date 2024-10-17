9930d3757ad04c1963b90618dd92933e6429630c
JoseVNucciR <jvnuccirodrigues@gmail.com>
2024-10-16 22:53:47 -0300

    Descrições

--- ATIV8/ATIV8_1e2.asm
+++ ATIV8/ATIV8_1e2.asm
@@ -3,33 +3,33 @@ TITLE Entrada de números binários
 .STACK 100h
 .CODE
 MAIN PROC
-    MOV CX,16
-    MOV AH,1
-    XOR BX,BX
-    INT 21h
+    MOV CX,16 ;carrega o contador com o valor de 16
+    MOV AH,1 ;função de leitura
+    XOR BX,BX ;zera o registrador BX
+    INT 21h ;lê o caracter do teclado
 TOPO:
-    CMP AL,0DH
+    CMP AL,0DH ;compara com o caracter "enter", se for igual vai para "SEGUE"
     JE SEGUE
-    AND AL,0FH
-    SHL BX,1
-    OR BL,AL
-    INT 21h
-    LOOP TOPO
+    AND AL,0FH ;transforma o caractere em número
+    SHL BX,1 ;desloca 1 bit a esquerda de BX
+    OR BL,AL ; executa or entre BL e AL
+    INT 21h  ;lê o caractere do teclado
+    LOOP TOPO ;decrementa CX e se não for zero repete TOPO
 SEGUE:
-    MOV CX,16
-    MOV AH,2
+    MOV CX,16 ;carrega o contador com o valor de 16
+    MOV AH,2 ;função para exibir caractere
 abra:
-    ROL BX,1
+    ROL BX,1 ;rola 1 bit mais significativo para o fim (move para a esquerda), se não houver carry pula para bulbassaur
     JNC bulbasaur
-    MOV DL,31h
-    INT 21h
+    MOV DL,31h ;se houver carry carrega o valor ASCII "1"
+    INT 21h ;exibe o caractere
     JMP charmander
 bulbasaur:
-    MOV DL,30h
-    INT 21h
+    MOV DL,30h ;carrega o valor "0" do ASCII
+    INT 21h ;exibe caractere "0" na tela
 charmander:
-    LOOP abra
-    MOV AH,4CH
+    LOOP abra ;decrementa CX até ser 0
+    MOV AH,4CH ;finaliza o programa
     INT 21h
 
     MOV AH,4CH
--- ATIV8/ATIV8_3.asm
+++ ATIV8/ATIV8_3.asm
@@ -3,24 +3,24 @@ TITLE Entrada de números hexadecimais
 .STACK 100h
 .CODE
 MAIN PROC
-    XOR BX,BX
-    MOV CL,4
-    MOV AH,1
+    XOR BX,BX ;zera o registrador BX
+    MOV CL,4 ;atribui o valor 4 ao contador CL
+    MOV AH,1 ;lê um caracter do teclado
     INT 21H
 TOPO:
-    CMP AL,0DH
+    CMP AL,0DH ;compara com o caracter "enter", se for igual vai para "FIM"
     JE FIM
-    CMP AL,39H
+    CMP AL,39H ;compara o número com "9", se for maior pula para "LETRA"
     JG LETRA 
-    AND AL,0FH
+    AND AL,0FH ;transforma o caractere em número 
     JMP DESLOCA
 LETRA:
-    SUB AL,37H
+    SUB AL,37H ;transforma o caractere em letra
 DESLOCA:
-    SHL BX,CL
-    OR BL,AL
-    INT 21H
-    JMP TOPO
+    SHL BX,CL ;desloca BX quatro bits para a esquerda
+    OR BL,AL ;faz um or entre BL e AL
+    INT 21H ;lê outro caractere digitado do teclado
+    JMP TOPO ;salta novamente para TOPO para repetir o processo e pegar outro caractere
 FIM:
     MOV AH,4CH
     INT 21H
